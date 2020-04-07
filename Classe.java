902da8824e8645602adff135b455139b278e18f1
Rodrigo Neto <rodnets4@gmail.com>
2020-04-06 23:45:36 -0300

    adicionando classe.java

---
+++ Classe.java
@@ -0,0 +1,12 @@
+public class Classe{
+    public int idade = 18;
+    public String nome = "rodrigo";
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public String getNome{
+        return this.nome;
+    }
+}
